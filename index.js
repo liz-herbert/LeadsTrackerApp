902d95d2c55f421dc767031b6489d3b95e979b20
Liz H. <137332408+liz-herbert@users.noreply.github.com>
2025-03-29 19:58:57 -0400

    Add comments to JS and HTML

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//import firebase config and functions from urls
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js"
 import { getDatabase,
          ref,
@@ -9,8 +10,11 @@ const firebaseConfig = {
     databaseURL: "https://leads-tracker-app-ac289-default-rtdb.firebaseio.com/"
 }
 
+//initialize the app with the firebaseconfig with url for database
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+
+//create reference inside database to push data to
 const referenceInDB = ref(database, "leads")
 
 const inputEl = document.getElementById("input-el")
@@ -32,21 +36,26 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+//onValue to fetch snapshot of database every time change is made to DB, only run if snapshot exists
 onValue(referenceInDB, function(snapshot) {
     const snapshotDoesExist = snapshot.exists()
     if (snapshotDoesExist) {
         const snapshotValues = snapshot.val()
+        //snapshot is in object form, needs to be converted to array
         const leads = Object.values(snapshotValues)
         render(leads)
     }
 })
 
 deleteBtn.addEventListener("dblclick", function() {
+    //remove data from the DB
     remove(referenceInDB)
     ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
+    //push value in input field to database
     push(referenceInDB, inputEl.value)
+    //remove input entries from HTML
     inputEl.value = "" 
 })
